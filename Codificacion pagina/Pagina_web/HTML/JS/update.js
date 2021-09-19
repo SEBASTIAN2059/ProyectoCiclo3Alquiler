5fa09b01d1a2c6c9a1f7dafc28b3260e30499864
SEBASTIAN2059 <sebastianlinares2059@gmail.com>
2021-09-19 12:08:44 -0500

    JS

---
+++ Codificacion pagina/Pagina_web/HTML/JS/update.js
@@ -0,0 +1,9 @@
+
+var type = document.getElementById("select_1").value;
+
+var type_text = document.getElementById("select_1");
+var selected = type_text.options[type_text.selectedIndex].text;
+
+document.write("<h1>"+type+"</h1>");
+
+document.write("<h1>"+selected+"</h1>");
