18cd63716b8878113be682c17ce89a45bc8356df
MykeStv <mykestv@gmail.com>
2021-10-12 15:11:09 -0500

    update

--- Codificacion pagina/Pagina_web/HTML/JS/alquiler.js
+++ Codificacion pagina/Pagina_web/HTML/JS/alquiler.js
@@ -19,6 +19,30 @@ function filtrarPlaca() {
             }
         }
     }
+} //end function filtrarPlaca()
+
+function filtrarTipo() {
+    let selectFilter = document.getElementById("tf-tipo");
+    let optionFilter = selectFilter.getElementsByTagName("option");
+    let filter = optionFilter.value.toUpperCase();
+    let table = document.getElementById("table-catalogo");
+    let tr = table.getElementsByTagName("tr");
+    let td;
+    let contValue;
+    let i;
+    for (i=0; i<tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1]; //column 1 refers to TIPO
+        if (td) {
+            contValue = td.innerText;
+            if (contValue.toUpperCase().indexOf(filter)>-1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+} //end function filtrarTipo()
 
 
-}
+//FILTER FOR TABLE SELECT ELEMENTS
+$('#table-catalogo tbody tr').show();
